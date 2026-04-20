5c8dd79949c9bb3ecd06b2817d57fd606bfdd782
Drey-ry <dreyredneverio5@gmail.com>
2026-04-20 11:56:36 +0800

    hello world

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Activitiest</title>
+</head>
+<body>
+    <div class="header">
+        <h1>These are my PHP Activities</h1>
+        <p>PHP is fun</p>
+    </div>
+</body>
+</html>
