ce4887dbffa23134ec99542df1009fa34da3d274
Drey-ry <dreyredneverio5@gmail.com>
2026-05-19 11:45:21 +0800

    Create personal.php

---
+++ SA2/3/personal.php
@@ -0,0 +1,6 @@
+<h3>Personal Information</h3>
+<ul>
+    <li><strong>Full Name:</strong> Dreyred Arenas</li>
+    <li><strong>Location:</strong> Metro Manila, Philippines</li>
+    <li><strong>Language proficiencies:</strong> English, Filipino</li>
+</ul>
