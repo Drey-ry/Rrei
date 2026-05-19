8f3127e3014302d7016ea9cb439dd4ad318cb289
Drey-ry <dreyredneverio5@gmail.com>
2026-05-19 11:45:23 +0800

    Create skills.php

---
+++ SA2/3/skills.php
@@ -0,0 +1,6 @@
+<h3>Technical Skills</h3>
+<ul>
+    <li>Web Environments: HTML5, CSS3, JavaScript </li>
+    <li>Back-end / Scripting Foundations: PHP Basics & Architecture</li>
+    <li>Responsive UI frameworks and systems design layout execution</li>
+</ul>
